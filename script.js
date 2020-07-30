826deb364f92d9b979eb180d045a2a1b21169981
JoannaTanveer <joanna.tanveer@gmail.com>
2020-07-30 15:16:39 -0700

    working with search function

---
+++ script.js
@@ -0,0 +1,93 @@
+var austinBtn = document.querySelector('.austin');
+var seattleBtn = document.querySelector('.seattle');
+var losangBtn = document.querySelector('.los-angeles');
+var newyorkBtn = document.querySelector('.new-york');
+var sanfranBtn = document.querySelector('.san-francisco');
+var chicagoBtn = document.querySelector('.chicago');
+var denverBtn = document.querySelector('.denver');
+var atlantaBtn = document.querySelector('.atlanta');
+
+
+var submitBtn = document.querySelector('.btn-info')
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+submitBtn.addEventListener("click", function (event){
+    event.preventDefault();
+
+    var searchElm = document.querySelector('.input-box')
+    console.log(searchElm.value, "search input from here")
+    var weatherDiv = document.querySelector('#weatherDiv')
+   
+    const key = '527517430ed7ab63b264a0ff97e01ae2'; 
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchElm.value}&appid=${key}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            console.log(json)
+            // Create all elements to put weather info in
+            var city = json.name;
+            var cityTitle = document.createElement('h2');
+            cityTitle.textContent=`Weather for ${ city  }`;
+            weatherDiv.append(cityTitle);
+
+            var temp = json.main.temp;
+            var tempF =parseInt((((temp - 273.15)*1.8)+32),10);
+            var tempFDisp=document.createElement('h1');
+            tempFDisp.textContent=`${tempF}\xB0F`;
+            weatherDiv.append(tempFDisp);
+            
+            var humid = json.main.humidity;
+            var humidityDisp = document.createElement('h3');
+            humidityDisp.textContent=`${humid}% humidity`;
+            weatherDiv.append(humidityDisp);
+
+            var wind= json.wind.speed
+            var windMPH =parseInt((wind*2.237),10);
+            var windDisp =document.createElement('h3');
+            windDisp.textContent=`${windMPH}MPH`;
+            weatherDiv.append(windDisp);
+
+
+            var lat=json.coord.lat;
+            var lon=json.coord.lon;
+            fetch(`http:api.openweathermap.org/data/2.5/uvi?appid=${ key }&lat=${ lat }&lon=${ lon }`)
+                .then(function(response) {
+                return response.json();
+            })
+                .then(function(jsonUV){
+                console.log(jsonUV);
+
+                var uv=jsonUV.value;
+                var uvDisp=document.createElement('h3');
+                uvDisp.textContent=`UV index ${uv}mW/m2`;
+                weatherDiv.append(uvDisp);
+            })
+        })
+
+
+
+    
+    
+
+
+
+    })
+})
+
+austinBtn.addEventListener('click', function (event){
+console.log(event, 'clicked');
+})
