4d81acbf9747333a48e52d32d9f8bcf528a761ea
JoannaTanveer <joanna.tanveer@gmail.com>
2020-08-09 13:51:18 -0700

    icons and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Weather
+This is a simple weather forcast generator utilizing the [OpenWeather API](https://openweathermap.org/api). Jquery, bootstrap and CSS were utilized. 
+
+https://joannatanveer.github.io/Weather/
+
+![Screenshot](calendar_screenshot.png)
--- script.js
+++ script.js
@@ -1,42 +1,66 @@
-var austinBtn = document.querySelector('.austin');
-var seattleBtn = document.querySelector('.seattle');
-var losangBtn = document.querySelector('.los-angeles');
-var newyorkBtn = document.querySelector('.new-york');
-var sanfranBtn = document.querySelector('.san-francisco');
-var chicagoBtn = document.querySelector('.chicago');
-var denverBtn = document.querySelector('.denver');
-var atlantaBtn = document.querySelector('.atlanta');
-
+var historyBtn = document.querySelectorAll('.btn');
 
 var submitBtn = document.querySelector('.btn-info')
+let storageArray = JSON.parse(localStorage.getItem('local'))
 
+let cityArray = storageArray || ['austin', 'seattle', 'los Angeles', 'new York', 'san Francisco', 'chicago', 'denver', 'atlanta']
 
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-
-
-submitBtn.addEventListener("click", function (event){
+//document.addEventListener('DOMContentLoaded', function() {
+$( document ).ready(function() {
+        
+    
+$('.btn-info').on('click', function (event){
+// submitBtn.addEventListener("click", function (event){
     event.preventDefault();
 
-    var searchElm = document.querySelector('.input-box')
-    console.log(searchElm.value, "search input from here")
-    var weatherDiv = document.querySelector('#weatherDiv')
+    var searchElm = document.querySelector('.input-box');
    
+   
+    weatherApi(searchElm.value)
+    cityArray.push(searchElm.value);
+    appendHistoryList(cityArray)
+    saveinLocalStorage("local", cityArray);
+})    
+
+
+function saveinLocalStorage (key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+   
+    }
+
+function appendHistoryList (array) {
+    $('.btn-group-vertical').html('');
+    for (i=0; i < array.length ; i++) {
+        let capitalize= array[i].charAt(0).toUpperCase() + array[i].slice(1)
+        
+        $('.btn-group-vertical').prepend (
+            `<button type="button" class="btn" data-city="${array[i]}" id= "cityBtn">${capitalize}</button>`
+        )
+    }
+}
+
+appendHistoryList(cityArray);
+
+$('.btn').on('click', function (event){
+    event.preventDefault();
+    console.log(event, 'clicked');
+    let cityBtn = $(this).attr("data-city");
+    console.log (cityBtn);
+    weatherApi(cityBtn);
+    
+    })
+
+function weatherApi (searchCity) {
     const key = '527517430ed7ab63b264a0ff97e01ae2'; 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchElm.value}&appid=${key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${key}`)
         .then(function(response) {
             return response.json();
         })
         .then(function(json) {
             console.log(json)
+           
+           $('#weatherDiv').html('');
+           $('.fiveDay').html('');
             // Create all elements to put weather info in
             var city = json.name;
             var date =moment().format("MM/DD/YY")
@@ -72,52 +96,62 @@ submitBtn.addEventListener("click", function (event){
                 console.log(jsonUV);
 
                 var uv=jsonUV.value;
+                
                 var uvDisp=document.createElement('h3');
                 uvDisp.textContent=`UV index:${uv}`;
                 weatherDiv.append(uvDisp);
             
-            fetch (`http://api.openweathermap.org/data/2.5/forecast?q=${searchElm.value}&appid=${key}`)
+                if (uv === 0 && uv <= 2.99){
+                    $(uvDisp).addClass('green')
+                
+                }
+                if (uv >= 3 && uv <= 5.99){
+                    $(uvDisp).addClass('yellow')
+                }
+                  
+                if (uv >= 6 && uv <= 7.99){
+                    $(uvDisp).addClass('orange')
+                }
+                if (uv >= 8 ){
+                    $(uvDisp).addClass('red')
+                }
+
+
+            fetch (`http://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${key}`)
                 .then(function(response){
                     return response.json();
         
                 })
                 .then(function(json5Day){
-                console.log(json5Day, "5day");
-
-                var locationIcon=document.querySelector('.weather-icon');
-                var icon=json5Day.list[0].weather[0].icon
-                console.log(icon, "icon code")
-                // var iconDisp=document.createElement('img');
-                locationIcon.innerHTML=`<img src="icons/${icon}.png">`;
-
-
-
-
-
-
+               
+                
+                for (i=0; i< 40; i+=8){
+                    var fiveDayTemp = parseInt((((json5Day.list[i].main.temp - 273.15)*1.8)+32),10);
+                    var icon=json5Day.list[i].weather[0].icon
+                    var humidity = json5Day.list[i].main.humidity;
+                    var date= moment.unix(json5Day.list[i].dt).format('MM/DD/YY')
+                    
+                    $('.fiveDay').append( 
+                        `<div class='weather-icon'><img src="icons/${icon}.png">
+                        <h2>${date} </h2>
+                        <h2>${fiveDayTemp}\xB0F ${humidity}% humidity </h2></div>`);
+                    
+  
+                   }
+                
+                
+                
 
                 })
             
             
-            
-            
-            
-            
-            
-            
             })
+                
         })
 
-   
-
-    
-    
+    }   
+  
 
 
+});
 
-    })
-})
-
-austinBtn.addEventListener('click', function (event){
-console.log(event, 'clicked');
-})
